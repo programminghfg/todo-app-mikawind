a6dc206eaa9f016d57737fae4120c38bf49c0fba
mikawind <mika.windheuser@hfg.design>
2023-05-17 10:30:00 +0200

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let todoText = '';
+    let todos = [];
+
+    function addToDo() {
+        todos.push({ text: todoText, done: false });
+        todos = todos;
+        todoText = '';
+    }
+
+    function remove(index) {
+        todos.splice(index, 1);
+        todos = todos;
+    }
+</script>
+
+<!--textfeld-->
+<input type="text" class="todo-input" bind:value={todoText} />
+<button class="Add"on:click={addToDo}>ADD</button>
+<!-- button add -->
+
+<!-- todo -->
+{#each todos as todo, index}
+    <div class="todo-entry" class:done={todo.done}>
+        <div>{todo.text}</div>
+        <input type="checkbox" bind:value={todo.done}/>
+        <button class="delete"on:click={(index) => {remove(index); }}>X</button>
+    </div>
+{/each}
+<!-- text -->
+<!-- checkboxen -->
+<style>
+    .done {
+        color: rgb(135, 135, 143);
+    }
+    .todo-entry{
+        display: flex;
+    }
+    .todo-input{
+        color: rgb(13, 13, 214);
+    }
+    .Add{
+        color: rgb(0, 139, 21);
+        font-size: medium;
+    }
+</style>
